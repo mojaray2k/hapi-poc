4119929d3cde6b33b7ac0f2133afcd64c34a3480
Amen Ra <ara@envistacorp.com>
2018-03-08 01:55:23 -0500

    feat: Showing how to use the Hapi Boom plugin
    The Hapi Boom plugin allows for more expressive way to write error code for https errors

---
+++ boom.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Hapi = require('hapi')
+const Boom = require('boom')
+
+const server = new Hapi.Server()
+
+server.connection({ host: 'localhost', port: 8001 })
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {   
+
+        reply(Boom.notFound())
+        /**
+         * The Following out put is printed out
+            HTTP/1.1 404 Not Found
+            Connection: keep-alive
+            Date: Thu, 08 Mar 2018 06:53:14 GMT
+            Transfer-Encoding: chunked
+            cache-control: no-cache
+            content-encoding: gzip
+            content-type: application/json; charset=utf-8
+            vary: accept-encoding
+
+            {
+                "error": "Not Found",
+                "statusCode": 404
+            }
+         */
+    }
+})
+/**
+ * You can make http request via the command line using HTTP PIE
+ * https://httpie.org
+ */
+
+
+server.start(() => {})
